01aa403f3d84ef7b822f2698e600b7246bb641a0
Matthew Dalby <matthew@matthewdalby.dev>
2025-01-18 09:37:42 -0800

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.module.scss'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>React/Teypescript/sass boilerplate</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
